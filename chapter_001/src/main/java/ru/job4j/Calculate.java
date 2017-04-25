fb9b41d5b3d1b4e2ab1d7df392073333ec78ef88
SergeyArtemov <sergeyartemov@yandex.ru>
2017-04-25 06:26:17 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
-
+/**
+* Class Класс для вычисления арифметических операций
+* @author sartemov
+* @since 25.04.2017
+* @version 1
+*/
 public class Calculate {
+/**
+* Метод main
+* @param args входная строка параметров
+* @return пустой тип void
+*/
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
