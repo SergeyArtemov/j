f24482c002973da9c7f0f45f8c7b65ce49bd4d7c
SergeyArtemov <sergeyartemov@yandex.ru>
2017-06-29 18:16:37 +0300

    task 6837

---
+++ chapter_001/src/main/java/ru/job4j/professions/Engineer.java
@@ -0,0 +1,37 @@
+package ru.job4j.professions;
+
+import java.util.ArrayList;
+
+/**
+ * Класс Инженер. Created by serge_000 on 29.06.2017.
+ */
+public class Engineer extends Profession {
+    private String speciality;
+    private String specialization;
+    public ArrayList<String> listOfProject;
+
+    public Engineer(){
+        listOfProject = new ArrayList<>();
+    }
+
+    public void workOnProject(String project){
+        listOfProject.add(project);
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+
+    public String getSpeciality() {
+        return speciality;
+    }
+}
