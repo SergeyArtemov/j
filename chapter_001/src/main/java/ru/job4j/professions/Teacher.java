22a34076ca11c941a62b76076744453736ad0f7e
SergeyArtemov <sergeyartemov@yandex.ru>
2017-06-29 18:20:04 +0300

    task 6837

---
+++ chapter_001/src/main/java/ru/job4j/professions/Teacher.java
@@ -0,0 +1,36 @@
+package ru.job4j.professions;
+
+import java.util.ArrayList;
+
+/**
+ * Класс Учитель. Created by serge_000 on 29.06.2017.
+ */
+public class Teacher extends Profession {
+    private String firstSubject;
+    private String secondSublect;
+    private ArrayList<String> listOfClass;
+
+    public Teacher(){
+        listOfClass = new ArrayList<>();
+    }
+
+    public String getFirstSubject() {
+        return firstSubject;
+    }
+
+    public void setFirstSubject(String firstSubject) {
+        this.firstSubject = firstSubject;
+    }
+
+    public String getSecondSublect() {
+        return secondSublect;
+    }
+
+    public void setSecondSublect(String secondSublect) {
+        this.secondSublect = secondSublect;
+    }
+
+    public void teachClass(String schoolClass){
+        listOfClass.add(schoolClass);
+    }
+}
