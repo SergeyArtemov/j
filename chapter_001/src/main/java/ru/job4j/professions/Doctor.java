16eb2b209f97474980d201edced5f5d2aa0227f1
SergeyArtemov <sergeyartemov@yandex.ru>
2017-06-29 18:13:14 +0300

    task 6837

---
+++ chapter_001/src/main/java/ru/job4j/professions/Doctor.java
@@ -0,0 +1,40 @@
+package ru.job4j.professions;
+
+import java.util.ArrayList;
+
+/**
+ * Created by serge_000 on 29.06.2017.
+ */
+public class Doctor extends Profession {
+    private String speciality;
+    private String specialization;
+    public ArrayList<String> listOfPatient;
+
+    public Doctor(){
+        listOfPatient = new ArrayList<>();
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public void treatPatient(String patient){
+        listOfPatient.add(patient);
+    }
+
+    public ArrayList<String> GiveAllPatients(){
+        return listOfPatient;
+    }
+}
